ea724918261d33765d2874cb80198f3059fbd2d6
Chamira Gunawardhana <chamiramadusankha96@gmail.com>
2024-07-05 10:15:05 +0530

    add whatsapp chat

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,18 @@
 import React from "react";
 import { FaFacebook, FaTwitter, FaInstagram, FaLinkedin } from "react-icons/fa";
+import { FaWhatsapp } from "react-icons/fa";
 
 const Footer = () => {
   return (
     <footer className="bg-gray-900 text-white p-4 fixed bottom-0 left-0 right-0">
+      <a
+        href="https://wa.me/+94712739650"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="fixed 2xl:bottom-20 2xl:right-36 lg:bottom-16 lg:right-6 md:bottom-16 md:right-3 bottom-28 right-8 bg-green-500 text-white p-4 rounded-full shadow-lg hover:bg-green-600 transition duration-300"
+      >
+        <FaWhatsapp size={32} />
+      </a>
       <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
         <div className="text-center md:text-left mb-4 md:mb-0">
           <p>

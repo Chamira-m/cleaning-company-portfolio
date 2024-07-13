8ab57a36188a027721c5f9f771a6ca5af66ed3c4
Chamira Gunawardhana <chamiramadusankha96@gmail.com>
2024-07-13 12:42:35 +0530

    change name steam to high presure

--- src/components/services/SteamCleaning.js
+++ src/components/services/SteamCleaning.js
@@ -59,7 +59,9 @@ const SteamCleaning = () => {
           className="w-full h-[400px] md:h-[500px] object-cover rounded-lg shadow-lg"
         />
         <div className="absolute inset-0 flex flex-col items-center justify-center text-white text-3xl bg-opacity-50 p-4 rounded-lg poppins-bold">
-          <h1 className="2xl:text-5xl md:text-3xl text-2xl">STEAM CLEANING</h1>
+          <h1 className="2xl:text-5xl md:text-3xl text-2xl">
+            High-Pressure Cleaning
+          </h1>
           <div className="2xl:w-[800px] md:w-[600px] w-80 h-1 bg-white mt-4 mb-4" />
         </div>
       </div>
